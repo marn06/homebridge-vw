ba3e0df97925221937248471a1879fd23d672dc9
Martin Lykke <martin.lykke@sgs.com>
2023-06-02 09:38:05 +0200

    release

--- src/car_states.py
+++ src/car_states.py
@@ -1,4 +1,9 @@
-from typing import TypedDict
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict, Literal, overload  # pylint: disable=no-name-in-module
+else:
+    from typing_extensions import TypedDict, Literal, overload
 from car_state import CarState
 
 
